ddab3de341c9549e6bdd18091a74ace84a5ee6c6
Y0ussef3del <youssefad888@gmail.com>
2026-07-05 02:07:29 +0300

    Finaly

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.whoamongus.khypqd"
+    applicationId = "com.youssef.meenfeena"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
